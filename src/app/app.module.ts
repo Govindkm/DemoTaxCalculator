b9a9886fa73b186da83516c63a1553a6d2c69486
Govind Kumar Mishra <govindkm.sv@gmail.com>
2023-03-21 21:32:08 +0530

    Adding css to give 3D effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { IncomeFormComponent } from './income-form/income-form.component';
 import { DeductionFormComponent } from './deduction-form/deduction-form.component';
