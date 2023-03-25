f3f0ef0a208412004f044f36a4be883e605d8a2c
Govind Kumar Mishra <govindkm.sv@gmail.com>
2023-03-25 13:58:04 +0530

    Testing Elastic Beans version

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://ec2-3-145-75-78.us-east-2.compute.amazonaws.com/api/'
+    apiUrl: 'http://calculatetaxapi.us-east-2.elasticbeanstalk.com/api/'
 };
