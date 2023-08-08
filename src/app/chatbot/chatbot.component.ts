3a6f86f383233bc78357b75c6de3c8a91616330c
Govind Kumar Mishra <govindkm.sv@gmail.com>
2023-08-08 12:07:26 +0530

    Adding chathistory to conversation

--- src/app/chatbot/chatbot.component.ts
+++ src/app/chatbot/chatbot.component.ts
@@ -5,27 +5,38 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-chatbot',
   templateUrl: './chatbot.component.html',
-  styleUrls: ['./chatbot.component.css']
+  styleUrls: ['./chatbot.component.css'],
 })
 export class ChatbotComponent {
-  messages: [{content: string, from: string}] = [{content: 'Hi, how can I help you?', from: 'ai'}];
+  messages: [{ content: string; from: string }] = [
+    { content: 'Hi, how can I help you?', from: 'ai' },
+  ];
   showChat = false;
   public userMessage = '';
   private apiURL = environment.apiUrl + 'chatbot/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   openChat() {
     this.showChat = this.showChat ? false : true;
   }
 
   async sendMessage(message: string) {
+    let chatHistory = '';
+    for (let i = 0; i < this.messages.length; i++) {
+      const m = this.messages[i];
+      chatHistory += `${m.from}: ${m.content}\n`;
+    }
+
     this.messages.push({ content: message, from: 'user' });
     this.userMessage = '';
     // Call your API here to get AI response
-    const aiResponse = await this.http.post( this.apiURL + 'askquestion', { question: message });
+    const aiResponse = await this.http.post(this.apiURL + 'askquestion', {
+      question: message,
+      chatHistory: chatHistory
+    });
     aiResponse.subscribe((data: any) => {
-    this.messages.push({ content: data.text, from: 'ai' });
+      this.messages.push({ content: data.text, from: 'ai' });
     });
   }
 }
